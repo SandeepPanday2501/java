c71dcdc6fc2741282cc635768a74fbe49ca90f59
SandeepPanday2501 <pandayshiroyasha@gmail.com>
2023-02-13 17:55:11 +0545

    Assignment

---
+++ Sandeep.java
@@ -0,0 +1,16 @@
+public class Sandeep {
+    public static void main(String[] args) {
+
+        System.out.println("Name of the organization:Kamalpokhari Resturant & Bar");
+        System.out.println("\n");
+        System.out.println("Address:");
+        System.out.println("----State: KTM");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println("\n");
+        System.out.println("Contact Infomation:");
+        System.out.println("----Mobile Number: 986532409");
+        System.out.println("----Email: kamalpokhari.rnb@gmail.com");
+
+    }
+}
